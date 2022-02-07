c68f18a59a58d99f6aef954880ac2e39d4f4a23d
ChairulAnamProgramming <chairuluniska@gmail.com>
2022-02-07 10:47:12 +0800

    V1

--- resources/views/backend/pages/reports/index.blade.php
+++ resources/views/backend/pages/reports/index.blade.php
@@ -9,6 +9,24 @@
                     <h4>Laporan/Report</h4>
                     <div class="list-group">
                         <a href="#" class="list-group-item list-group-item-action">
+                            Laporan Rekap Data Pendidikan
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            Laporan Rekap Data Kecamatan
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            Laporan Rekap Data Jurusan
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            Laporan Rekap Data Jenis Kelamin
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            Laporan Rekap Data Lowongan Kerja
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action disabled">
+                            Laporan Data Pencari Kerja
+                        </a>
+                        {{-- <a href="#" class="list-group-item list-group-item-action">
                             Laporan Data Pengalaman Kerja
                         </a>
                         <a href="#" class="list-group-item list-group-item-action">
@@ -22,7 +40,7 @@
                         </a>
                         <a href="#" class="list-group-item list-group-item-action disabled">
                             Laporan Data Pelatihan Kerja
-                        </a>
+                        </a> --}}
                     </div>
                 </div>
             </div>
--- resources/views/backend/template/inc/side.blade.php
+++ resources/views/backend/template/inc/side.blade.php
@@ -11,56 +11,60 @@
                 <span class="menu-title">Beranda</span>
             </a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false"
-                aria-controls="ui-basic">
-                <i class="mdi mdi-palette menu-icon"></i>
-                <span class="menu-title">Data Master</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="ui-basic">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('district.index') }}">
-                            Kecamatan
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('education.index') }}">
-                            Pendidikan
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('population.index') }}">
-                            Masyarakat
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('company.index') }}">
-                            Perusahaan
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('job_seeker.index') }}">
-                <i class="mdi mdi-briefcase menu-icon"></i>
-                <span class="menu-title">Pencari Kerja</span>
-            </a>
-        </li>
-        {{-- <li class="nav-item">
-            <a class="nav-link" href="{{ route('work_training.index') }}">
-                <i class="mdi mdi-worker menu-icon"></i>
-                <span class="menu-title">Pelatihan Kerja</span>
-            </a>
-        </li> --}}
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('job_vacancy.index') }}">
-                <i class="mdi mdi-file-document-box menu-icon"></i>
-                <span class="menu-title">Lowongan Kerja</span>
-            </a>
-        </li>
+        @if (Auth::user()->rule === 'admin')
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false"
+                    aria-controls="ui-basic">
+                    <i class="mdi mdi-palette menu-icon"></i>
+                    <span class="menu-title">Data Master</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="collapse" id="ui-basic">
+                    <ul class="nav flex-column sub-menu">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('district.index') }}">
+                                Kecamatan
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('education.index') }}">
+                                Pendidikan
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('population.index') }}">
+                                Masyarakat
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('company.index') }}">
+                                Perusahaan
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        @endif
+        @if (Auth::user()->rule === 'admin')
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('job_seeker.index') }}">
+                    <i class="mdi mdi-briefcase menu-icon"></i>
+                    <span class="menu-title">Pencari Kerja</span>
+                </a>
+            </li>
+            {{-- <li class="nav-item">
+                <a class="nav-link" href="{{ route('work_training.index') }}">
+                    <i class="mdi mdi-worker menu-icon"></i>
+                    <span class="menu-title">Pelatihan Kerja</span>
+                </a>
+            </li> --}}
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('job_vacancy.index') }}">
+                    <i class="mdi mdi-file-document-box menu-icon"></i>
+                    <span class="menu-title">Lowongan Kerja</span>
+                </a>
+            </li>
+        @endif
         <li class="nav-item">
             <a class="nav-link" href="{{ route('job_application.index') }}">
                 <i class="mdi mdi mdi-worker menu-icon"></i>
@@ -73,11 +77,13 @@
                 <span class="menu-title">Biodata</span>
             </a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('reports.index') }}">
-                <i class="mdi mdi-database menu-icon"></i>
-                <span class="menu-title">Laporan</span>
-            </a>
-        </li>
+        @if (Auth::user()->rule === 'admin')
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('reports.index') }}">
+                    <i class="mdi mdi-database menu-icon"></i>
+                    <span class="menu-title">Laporan</span>
+                </a>
+            </li>
+        @endif
     </ul>
 </nav>
